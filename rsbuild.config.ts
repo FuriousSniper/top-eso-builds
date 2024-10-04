e4ddf67b59c7b7724bc42362c90f8192b033caab
Michal S <m.sarota@demoboost.com>
2024-10-04 13:11:31 +0200

    fix to meta values

--- rsbuild.config.ts
+++ rsbuild.config.ts
@@ -6,6 +6,12 @@ export default defineConfig({
   plugins: [pluginReact(), pluginLess()],
   html: {
     favicon: './public/icons/logo/topesologo-icon-tiny.png',
-    title: "Top ESO Builds"
+    title: "Top ESO Builds",
+    meta:{
+      description: "The only source of top ESO builds",
+      site_name: "Top ESO Builds",
+      image: "./public/icons/logo/topesologo-icon.png",
+      themeColor:"#0d217d"
+    }
   },
 });
