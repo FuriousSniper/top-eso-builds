a1820ee7fd1be4ce78018e9d4c5a217c403c0092
Michal S <m.sarota@demoboost.com>
2025-06-26 16:45:51 +0200

    feat: added filtering of debuffs, saving skills in url, copying skills as img

--- src/components/Sidebar/SubclassingSidebarLeft/index.tsx
+++ src/components/Sidebar/SubclassingSidebarLeft/index.tsx
@@ -1,6 +1,7 @@
-import { useRef } from 'react'
-import './style.less'
+import { useRef, useState } from 'react'
+import * as _ from "lodash";
 import Sidebar from '..'
+import './style.less'
 
 interface SubclassingSidebarProps {
     toggleFunction: (debuff: string) => void,
@@ -8,7 +9,9 @@ interface SubclassingSidebarProps {
 }
 
 const SubclassingSidebarLeft = (props: SubclassingSidebarProps) => {
-    const ref = useRef<HTMLFormElement>(null)
+    const [debuffName, setDebuffName] = useState<string>('')
+    const ref = useRef<HTMLFormElement>(null)    
+    const inputRef = useRef<HTMLInputElement>(null)
     const debuffs = { 'Major': ['Aegis', 'Berserk', 'Breach', 'Brittle', 'Brutality', 'Courage', 'Cowardice', 'Defile', 'Endurance', 'Enervation', 'Evasion', 'Expedition', 'Force', 'Fortitude', 'Heroism', 'Intellect', 'Lifesteal', 'Magickasteal', 'Maim', 'Mangle', 'Mending', 'Prophecy', 'Protection', 'Resolve', 'Savagery', 'Slayer', 'Sorcery', 'Timidity', 'Toughness', 'Uncertainty', 'Vitality', 'Vulnerability'], 'Minor': ['Aegis', 'Berserk', 'Breach', 'Brittle', 'Brutality', 'Courage', 'Cowardice', 'Defile', 'Endurance', 'Enervation', 'Evasion', 'Expedition', 'Force', 'Fortitude', 'Gallop', 'Heroism', 'Intellect', 'Lifesteal', 'Magickasteal', 'Maim', 'Mangle', 'Mending', 'Prophecy', 'Protection', 'Resolve', 'Savagery', 'Slayer', 'Sorcery', 'Timidity', 'Toughness', 'Uncertainty', 'Vitality', 'Vulnerability'], 'Other': ['Empower'] }
 
     const resetDebuffs = () => {
@@ -17,6 +20,21 @@ const SubclassingSidebarLeft = (props: SubclassingSidebarProps) => {
         checkboxes?.forEach((checkbox: HTMLInputElement) => {
             checkbox.checked = false
         });
+    }
+
+    const handleChange = _.debounce(function () {
+        if (!inputRef.current) {
+            return
+        }
+        setDebuffName(inputRef.current?.value)
+    }, 300)
+
+    const deleteDebuffName = () => {
+        setDebuffName('')
+        if (!inputRef.current) {
+            return
+        }
+        inputRef.current.value=""
     }
 
     const determineEffect = (category: string, effect: string) => {
@@ -32,6 +50,9 @@ const SubclassingSidebarLeft = (props: SubclassingSidebarProps) => {
             <form ref={ref}>
                 <h4 className="sidebarTitle">Effects</h4>
                 <div className="resetDebuffsWrapper">
+                    <label htmlFor="debuffName"><span>Debuff name</span><div><input type="text" id='debuffName' ref={inputRef} onChange={handleChange} /><button type='button' onClick={deleteDebuffName}>X</button></div></label>
+                </div>
+                <div className="resetDebuffsWrapper">
                     <button onClick={resetDebuffs} type='button' className='resetDebuffsButton'>Reset</button>
                 </div>
                 {
@@ -41,12 +62,14 @@ const SubclassingSidebarLeft = (props: SubclassingSidebarProps) => {
                                 <div className='category' key={_key}>{value[0]}</div>
                                 {
                                     value[1].map((effect: string, key: number) => {
-                                        return (
-                                            <label className="effect" key={key}>
-                                                <span>{effect}</span>
-                                                <input type="checkbox" name="effects" id={`effect_${key}`} onChange={() => props.toggleFunction(determineEffect(value[0], effect))} />
-                                            </label>
-                                        )
+                                        if (effect.toLowerCase().includes(debuffName.toLowerCase())) {
+                                            return (
+                                                <label className="effect" key={key}>
+                                                    <span>{effect}</span>
+                                                    <input type="checkbox" name="effects" id={`effect_${key}`} onChange={() => props.toggleFunction(determineEffect(value[0], effect))} />
+                                                </label>
+                                            )
+                                        }
                                     })
                                 }
                             </>
